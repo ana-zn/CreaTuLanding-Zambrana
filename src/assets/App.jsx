a7803d8b59ccf0dd2dbd9285ea30d4d335432939
Ana Zambrana <113066266+ana-zn@users.noreply.github.com>
2025-02-19 18:39:34 -0300

    Se creo el archivo items

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css';  
 import NavBar from '../components/navbar/navbar';
-import SectionHero from '../components/sectionhero/SectinHero'
+import SectionHero from '../components/sectionhero/SectinHero'; 
+import ItemlistContainer from '../components/itemlistcontainer/ItemListContainer'; 
 
 function App() {
     return (
         <>
             <NavBar />
             <SectionHero />
-            <div></div>
+            <ItemlistContainer />
 
         </>
     ); 
